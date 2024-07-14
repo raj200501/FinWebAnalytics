9f314c18d654348536fbf86818aa64b67c04af74
Raj Kashikar <65059626+raj200501@users.noreply.github.com>
2024-07-14 00:07:57 -0700

    Create WebApp.fs

---
+++ src/FinWebAnalytics/WebApp.fs
@@ -0,0 +1,23 @@
+module WebApp
+
+open Giraffe
+open Giraffe.ViewEngine
+
+let index =
+    html [] [
+        head [] [
+            title [] [ str "FinWebAnalytics" ]
+            link [ _rel "stylesheet"; _type "text/css"; _href "/Styles.css" ]
+        ]
+        body [] [
+            h1 [] [ str "Welcome to FinWebAnalytics" ]
+            form [ _action "/analyze"; _method "post" ] [
+                input [ _type "text"; _name "data"; _placeholder "Enter financial data" ]
+                button [ _type "submit" ] [ str "Analyze" ]
+            ]
+            form [ _action "/predict"; _method "post" ] [
+                input [ _type "text"; _name "data"; _placeholder "Enter financial data" ]
+                button [ _type "submit" ] [ str "Predict" ]
+            ]
+        ]
+    ]
